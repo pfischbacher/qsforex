bcab31d1dea6111fc11147573919c52662c69bde
pfischbacher <pfischbacher@gmail.com>
2016-05-29 22:09:53 +0900

    modified portofio.py, position.py

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -15,68 +15,35 @@ from qsforex.settings import OUTPUT_RESULTS_DIR
 
 class Portfolio(object):
     def __init__(
-        self, ticker, events, home_currency="GBP", 
-        leverage=20, equity=Decimal("100000.00"), 
+        self, home_currency="USD", 
+        leverage=20, equity=100000.00, 
         risk_per_trade=Decimal("0.02"), backtest=True
     ):
-        self.ticker = ticker
-        self.events = events
-        self.home_currency = home_currency
+        self.home_currency = home_currency  # Account denomination (e.g. GBP)
         self.leverage = leverage
-        self.equity = equity
+        self.equity = Decimal(equity)
         self.balance = deepcopy(self.equity)
         self.risk_per_trade = risk_per_trade
         self.backtest = backtest
-        self.trade_units = self.calc_risk_position_size()
-        self.positions = {}
+        self.order_num = 0
+        self.pairs = []
+        self.trade_lots = self.calc_risk_position_size()
+        #self.positions = defaultdict(list)
         if self.backtest:
             self.backtest_file = self.create_equity_file()
         self.logger = logging.getLogger(__name__)
 
+    def add_pair(self, pair):
+        self.pairs.append(pair)
+        
     def calc_risk_position_size(self):
         return self.equity * self.risk_per_trade
-
-    def add_new_position(
-        self, position_type, currency_pair, units, ticker
-    ):
-        ps = Position(
-            self.home_currency, position_type, 
-            currency_pair, units, ticker
-        )
-        self.positions[currency_pair] = ps
-
-    def add_position_units(self, currency_pair, units):
-        if currency_pair not in self.positions:
-            return False
-        else:
-            ps = self.positions[currency_pair]
-            ps.add_units(units)
-            return True
-
-    def remove_position_units(self, currency_pair, units):
-        if currency_pair not in self.positions:
-            return False
-        else:
-            ps = self.positions[currency_pair]
-            pnl = ps.remove_units(units)
-            self.balance += pnl
-            return True
-
-    def close_position(self, currency_pair):
-        if currency_pair not in self.positions:
-            return False
-        else:
-            ps = self.positions[currency_pair]
-            pnl = ps.close_position()
-            self.balance += pnl
-            del[self.positions[currency_pair]]
-            return True
 
     def create_equity_file(self):
         filename = "backtest.csv"
         out_file = open(os.path.join(OUTPUT_RESULTS_DIR, filename), "w")
-        header = "Timestamp,Balance"
-        for pair in self.ticker.pairs:
+        header = "Order,Pair,Open Time,Close Time,PNL,lots,Balance,Position Type,Close Type,Take Profit,Stop Loss,Trailing Stop"
+        for pair in self.pairs:
             header += ",%s" % pair
         header += "\n"
         out_file.write(header)
@@ -108,12 +75,20 @@ class Portfolio(object):
         
         print("Simulation complete and results exported to %s" % out_filename)
 
-    def update_portfolio(self, tick_event):
+    def update_portfolio(self, pair, position, extra=""):
+        self.balance += position.pnl
+        self.order_num += 1
+        if self.backtest:
+            out_line = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (self.order_num, pair, position.open_time, position.close_time, position.pnl, position.lots, self.balance, position.position_type, position.close_type, position.take_profit, position.stop_loss, position.trailing_stop, extra)
+            out_line += "\n"
+            print(out_line[:-2])
+            self.backtest_file.write(out_line)
         """
         This updates all positions ensuring an up to date
         unrealised profit and loss (PnL).
         """
-        currency_pair = tick_event.instrument
+        
+        """currency_pair = tick_event.instrument
         if currency_pair in self.positions:
             ps = self.positions[currency_pair]
             ps.update_position_price()
@@ -126,9 +101,11 @@ class Portfolio(object):
                     out_line += ",0.00"
             out_line += "\n"
             print(out_line[:-2])
-            self.backtest_file.write(out_line)
+            self.backtest_file.write(out_line)"""
+        pass
 
-    def execute_signal(self, signal_event):
+    #Going to move this out of the portfolio class, strategy class or new order class?
+    """def execute_signal(self, signal_event):
         # Check that the prices ticker contains all necessary
         # currency pairs prior to executing an order
         execute = True
@@ -142,7 +119,7 @@ class Portfolio(object):
         if execute:
             side = signal_event.side
             currency_pair = signal_event.instrument
-            units = int(self.trade_units)
+            lots = int(self.trade_lots)
             time = signal_event.time
             
             # If there is no position, create one
@@ -151,43 +128,47 @@ class Portfolio(object):
                     position_type = "long"
                 else:
                     position_type = "short"
+                    
+                ps = Position(
+                    position_type, currency_pair, lots, tick, current_index+1
+                )
                 self.add_new_position(
-                    position_type, currency_pair, 
-                    units, self.ticker
+                    ps
                 )
 
-            # If a position exists add or remove units
+            # If a position exists add or remove lots
             else:
                 ps = self.positions[currency_pair]
 
                 if side == "buy" and ps.position_type == "long":
-                    add_position_units(currency_pair, units)
+                    self.add_position_lots(currency_pair, lots)
 
                 elif side == "sell" and ps.position_type == "long":
-                    if units == ps.units:
+                    if lots == ps.lots:
                         self.close_position(currency_pair)
-                    # TODO: Allow units to be added/removed
-                    elif units < ps.units:
+                    # TODO: Allow lots to be added/removed
+                    elif lots < ps.lots:
                         return
-                    elif units > ps.units:
+                    elif lots > ps.lots:
                         return
 
                 elif side == "buy" and ps.position_type == "short":
-                    if units == ps.units:
+                    if lots == ps.lots:
                         self.close_position(currency_pair)
-                    # TODO: Allow units to be added/removed
-                    elif units < ps.units:
+                    # TODO: Allow lots to be added/removed
+                    elif lots < ps.lots:
                         return
-                    elif units > ps.units:
+                    elif lots > ps.lots:
                         return
                         
                 elif side == "sell" and ps.position_type == "short":
-                    add_position_units(currency_pair, units)
+                    self.add_position_lots(currency_pair, lots)
 
-            order = OrderEvent(currency_pair, units, "market", side)
+            order = OrderEvent(currency_pair, lots, "market", side)
             self.events.put(order)
 
             self.logger.info("Portfolio Balance: %s" % self.balance)
         else:
             self.logger.info("Unable to execute order as price data was insufficient.")
         
+        """
--- portfolio/position.py
+++ portfolio/position.py
@@ -1,33 +1,44 @@
-from decimal import Decimal, getcontext, ROUND_HALF_DOWN
-
+from decimal import Decimal, getcontext, ROUND_HALF_DOWN, ROUND_HALF_UP
+from qsforex import settings
 
 class Position(object):
     def __init__(
-        self, home_currency, position_type, 
-        currency_pair, units, ticker
+        self, pair, position_type, open_tick, lots, position_index,
+        take_profit = 0, stop_loss = 0, trailing_stop = 0
     ):
-        self.home_currency = home_currency  # Account denomination (e.g. GBP)
+        #self.home_currency = home_currency  # Account denomination (e.g. GBP)
         self.position_type = position_type  # Long or short
-        self.currency_pair = currency_pair  # Intended traded currency pair
-        self.units = units
-        self.ticker = ticker
+        self.pair = pair  # Intended traded currency pair
+        self.open_time = open_tick.time
+        self.open_price = self.get_price(open_tick)
+        self.lots = lots
+        self.index = position_index
+        self.take_profit = take_profit
+        self.stop_loss = stop_loss
+        self.trailing_stop = trailing_stop
         self.set_up_currencies()
-        self.profit_base = self.calculate_profit_base()
-        self.profit_perc = self.calculate_profit_perc()
-
-    def set_up_currencies(self):
-        self.base_currency = self.currency_pair[:3]    # For EUR/USD, this is EUR
-        self.quote_currency = self.currency_pair[3:]   # For EUR/USD, this is USD
-        # For EUR/USD, with account denominated in GBP, this is USD/GBP
-        self.quote_home_currency_pair = "%s%s" % (self.quote_currency, self.home_currency)
-
-        ticker_cur = self.ticker.prices[self.currency_pair]
+        self.state = "open"
+        self.start_trailing_stop = False
+        self.ticksize = Decimal("0.00001")
+        self.init_conditions()
+        #self.profit_base = self.calculate_profit_base()
+        #self.profit_perc = self.calculate_profit_perc()
+        
+        
+    def get_price(self, tick):
+        result = 0
         if self.position_type == "long":
-            self.avg_price = Decimal(str(ticker_cur["ask"]))
-            self.cur_price = Decimal(str(ticker_cur["bid"]))    
-        else:
-            self.avg_price = Decimal(str(ticker_cur["bid"]))
-            self.cur_price = Decimal(str(ticker_cur["ask"]))
+            result = tick.ask
+        elif self.position_type == "short":
+            result = tick.bid
+        return result
+        
+    def set_up_currencies(self):
+        self.base_currency = self.pair[:3]    # For EUR/USD, this is EUR
+        self.quote_currency = self.pair[3:]   # For EUR/USD, this is USD
+
+    def number_units(self):
+        return int(settings.LOTSIZE * self.lots)
 
     def calculate_pips(self):
         mult = Decimal("1")
@@ -38,72 +49,162 @@ class Position(object):
         pips = (mult * (self.cur_price - self.avg_price)).quantize(
             Decimal("0.00001"), ROUND_HALF_DOWN
         )
+        
         return pips
 
     def calculate_profit_base(self):
         pips = self.calculate_pips()
-        ticker_qh = self.ticker.prices[self.quote_home_currency_pair]
-        if self.position_type == "long":
-            qh_close = ticker_qh["bid"]
-        else:
-            qh_close = ticker_qh["ask"]
-        profit = pips * qh_close * self.units
-        return profit.quantize(
-            Decimal("0.00001"), ROUND_HALF_DOWN
-        )   
+        qh_close = 1
+        #ticker_qh = self.ticker.prices[self.quote_home_pair]
+        ticker_qh = self.ticker.prices.get(self.quote_home_pair, None)
+        if ticker_qh != None:
+            if self.position_type == "long":
+                qh_close = ticker_qh["bid"]
+            else:
+                qh_close = ticker_qh["ask"]
+        profit = pips * qh_close * self.lots
+        return profit.quantize(self.ticksize), ROUND_HALF_DOWN
 
     def calculate_profit_perc(self):
-        return (self.profit_base / self.units * Decimal("100.00")).quantize(
-            Decimal("0.00001"), ROUND_HALF_DOWN
+        return (
+            self.profit_base / self.lots * Decimal("100.00")).quantize(self.ticksize, ROUND_HALF_DOWN
         )
+    
+    def init_conditions(self):
+        self.set_stop_loss()
+        self.set_take_profit()
+        
+    def set_stop_loss(self):
+        if self.stop_loss > 0:
+            if self.position_type == "long":
+                self.stop_loss_limit = self.open_price - self.stop_loss * self.ticksize
+            elif self.position_type == "short":
+                self.stop_loss_limit = self.open_price + self.stop_loss * self.ticksize
+        
+    def set_take_profit(self):
+        if self.take_profit > 0:
+            if self.position_type == "long":
+                self.take_profit_limit = self.open_price + self.take_profit * self.ticksize
+            elif self.position_type == "short":
+                    self.take_profit_limit = self.open_price - self.take_profit * self.ticksize
+    
+    def take_profit_trigger(self, tick):
+        if self.take_profit_limit is not None:
+            if self.position_type == "long":
+                if tick.bid >= self.take_profit_limit:
+                    self.close_position(tick.bid, tick.time, "Take Profit")
+            elif self.position_type == "short":
+                if tick.ask <= self.take_profit_limit:
+                    self.close_position(tick.ask, tick.time, "Take Profit")
+        
+    def stop_loss_trigger(self, tick):
+        if self.start_trailing_stop:
+            label = "Trailing Stop"
+        else:
+            label = "Stop Loss"
+        if self.stop_loss_limit is not None:
+            if self.position_type == "long":
+                if tick.bid <= self.stop_loss_limit:
+                    self.close_position(tick.bid, tick.time, label)
+            elif self.position_type == "short":
+                if tick.ask >= self.stop_loss_limit:
+                    self.close_position(tick.ask, tick.time, label)
 
-    def update_position_price(self):
-        ticker_cur = self.ticker.prices[self.currency_pair]
+    def set_trailing_stop(self, tick):
+        if self.start_trailing_stop:
+            if self.position_type == "long":
+                if tick.bid - self.trailing_stop * self.ticksize > self.stop_loss_limit:
+                    self.stop_loss_limit = Decimal(tick.bid - self.trailing_stop * self.ticksize).quantize(self.ticksize, ROUND_HALF_DOWN)
+                   
+            elif self.position_type == "short":
+                if tick.ask + self.trailing_stop * self.ticksize < self.stop_loss_limit:
+                    self.stop_loss_limit = Decimal(tick.ask  + self.trailing_stop * self.ticksize).quantize(self.ticksize, ROUND_HALF_UP)
+                    
+        elif self.trailing_stop > 0:
+            if self.position_type == "long":
+                if tick.bid - self.open_price > self.trailing_stop * self.ticksize:
+                    self.start_trailing_stop = True
+                    self.stop_loss_limit = Decimal(tick.bid - self.trailing_stop * self.ticksize).quantize(self.ticksize, ROUND_HALF_UP)
+                
+            elif self.position_type == "short":
+                if self.open_price - tick.ask > self.trailing_stop * self.ticksize:
+                    self.start_trailing_stop = True
+                    self.stop_loss_limit = (tick.ask + self.trailing_stop * self.ticksize).quantize(self.ticksize)
+                
+            #print('position.py', 'Trailing Stop', self.trailing_stop, 'Stop Loss', self.stop_loss)
+        
+    def update_position(self, tick):
+        self.update_conditions(tick)
+        self.set_trailing_stop(tick)
+        self.take_profit_trigger(tick)
+        self.stop_loss_trigger(tick)
+
+    def update_conditions(self, tick):
+        if self.position_type == "long":
+            price = tick.bid
+            self.unrealize_pnl = Decimal((tick.bid - self.open_price) * self.number_units()).quantize(Decimal("0.0001"), ROUND_HALF_DOWN)
+        elif self.position_type == "short":
+            price = tick.ask
+            self.unrealize_pnl = Decimal((self.open_price - tick.ask) * self.number_units()).quantize(Decimal("0.0001"), ROUND_HALF_DOWN)
+     
+    
+    def update_position_price(self, tick):
+        """ticker_cur = self.ticker.prices[self.pair]
         if self.position_type == "long":
             self.cur_price = Decimal(str(ticker_cur["bid"]))
         else:
             self.cur_price = Decimal(str(ticker_cur["ask"]))
         self.profit_base = self.calculate_profit_base()
-        self.profit_perc = self.calculate_profit_perc()
-
-    def add_units(self, units):
-        cp = self.ticker.prices[self.currency_pair]
+        self.profit_perc = self.calculate_profit_perc()"""
+        pass
+    
+    def close_position(self, price, time, close_type):
+        self.close_price = price
+        self.close_time = time
+        self.close_type = close_type
+        self.pnl = self.unrealize_pnl
+        #print("Position.py", "pnl", self.pnl, "Close Type", close_type)   
+        self.state = "closed"
+        self.unrealize_pnl = 0
+            
+        """self.update_position_price()
+        # Calculate PnL
+        pnl = self.calculate_pips() * qh_close * self.lots
+        getcontext().rounding = ROUND_HALF_DOWN
+        return pnl.quantize(Decimal("0.01"))"""
+    
+    #Not sure how to add/remove lots to a position, need to look into this more.    
+    """
+    def add_lots(self, lots):
+        cp = self.ticker.prices[self.pair]
         if self.position_type == "long":
             add_price = cp["ask"]
         else:
             add_price = cp["bid"]
-        new_total_units = self.units + units
-        new_total_cost = self.avg_price*self.units + add_price*units
-        self.avg_price = new_total_cost/new_total_units
-        self.units = new_total_units
+        new_total_lots = self.lots + lots
+        new_total_cost = self.avg_price*self.lots + add_price*lots
+        self.avg_price = new_total_cost/new_total_lots
+        self.lots = new_total_lots
         self.update_position_price()
 
-    def remove_units(self, units):
-        dec_units = Decimal(str(units))
-        ticker_cp = self.ticker.prices[self.currency_pair]
-        ticker_qh = self.ticker.prices[self.quote_home_currency_pair]
+    def remove_lots(self, lots):
+        dec_lots = Decimal(str(lots))
+        qh_close = 1
+        ticker_cp = self.ticker.prices[self.pair]
+        ticker_qh = self.ticker.prices.get(self.quote_home_pair, None)
         if self.position_type == "long":
             remove_price = ticker_cp["bid"]
-            qh_close = ticker_qh["ask"]
+            if ticker_qh != None:
+                qh_close = ticker_qh["ask"]
         else:
             remove_price = ticker_cp["ask"]
-            qh_close = ticker_qh["bid"]
-        self.units -= dec_units
+            if ticker_qh != None:
+                qh_close = ticker_qh["bid"]
+    
+        self.lots -= dec_lots
         self.update_position_price()
         # Calculate PnL
-        pnl = self.calculate_pips() * qh_close * dec_units
+        pnl = self.calculate_pips() * qh_close * dec_lots
         getcontext().rounding = ROUND_HALF_DOWN
         return pnl.quantize(Decimal("0.01"))
-
-    def close_position(self):
-        ticker_cp = self.ticker.prices[self.currency_pair]
-        ticker_qh = self.ticker.prices[self.quote_home_currency_pair]
-        if self.position_type == "long":
-            qh_close = ticker_qh["ask"]
-        else:
-            qh_close = ticker_qh["bid"]
-        self.update_position_price()
-        # Calculate PnL
-        pnl = self.calculate_pips() * qh_close * self.units
-        getcontext().rounding = ROUND_HALF_DOWN
-        return pnl.quantize(Decimal("0.01"))
+    """
